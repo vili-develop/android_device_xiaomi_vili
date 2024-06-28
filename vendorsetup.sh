2659d2bb5e695bc33447feb4d7c4f2d96d4d4195
Omar Aguilar <omarag9099@gmail.com>
2024-06-28 01:31:40 -0600

    vili: QPR3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ git clone https://github.com/0mar99/android_device_xiaomi_sm8350-common.git -b d
 
 # Kernel
 echo 'Cloning kernel'
-git clone https://github.com/0mar99/android_kernel_xiaomi_sm8350.git --recursive -b lineage-21 kernel/xiaomi/sm8350
+git clone https://github.com/0mar99/android_kernel_xiaomi_sm8350.git -b lineage-21 kernel/xiaomi/sm8350
 
 # Hardware Xiaomi
 echo 'Cloning hardware xiaomi'
@@ -19,7 +19,7 @@ git clone https://github.com/0mar99/proprietary_vendor_xiaomi_vili.git -b lineag
 
 # Vendor common
 echo 'Cloning common vendor tree'
-git clone https://github.com/0mar99/proprietary_vendor_xiaomi_sm8350-common.git -b lineage-21 vendor/xiaomi/sm8350-common
+git clone https://github.com/0mar99/proprietary_vendor_xiaomi_sm8350-common.git -b derp vendor/xiaomi/sm8350-common
 
 # Leica camera
 echo 'Cloning Leica camera'
@@ -28,5 +28,21 @@ git clone https://gitlab.com/0mar99/proprietary_vendor_xiaomi_camera.git -b leic
 # Firmware
 echo 'Cloning firmware'
 git clone --depth=1 https://gitlab.com/0mar99/vili-firmware.git -b global vendor/xiaomi/vili-firmware
+
+# HALs
+echo 'Cloning updated HALs'
+rm -rf hardware/qcom-caf/sm8350/display
+git clone --depth=1 https://github.com/0mar99/hardware_qcom_display.git -b 14-caf-sm8350 hardware/qcom-caf/sm8350/display
+
+rm -rf hardware/qcom-caf/sm8350/media
+git clone --depth=1 https://github.com/0mar99/hardware_qcom_media.git -b 14-caf-sm8350 hardware/qcom-caf/sm8350/media
+
+rm -rf hardware/qcom-caf/sm8350/audio
+git clone --depth=1 https://github.com/0mar99/hardware_qcom_audio.git -b 14-caf-sm8350 hardware/qcom-caf/sm8350/audio
+
+# Keys
+echo 'Cloning private keys'
+rm -rf vendor/derp/signing
+git clone https://gitlab.com/0mar99/vendor_derp_signing.git vendor/derp/signing
 
 echo 'delete vendorsetup.sh from device tree once this is done'
